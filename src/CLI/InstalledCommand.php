7a93c8276e32cffee1ca61851d323d26c45c4772
Torsten Dittmann <torsten.dittmann@googlemail.com>
2023-10-22 21:00:35 +0200

    fix: port

--- src/CLI/InstalledCommand.php
+++ src/CLI/InstalledCommand.php
@@ -4,20 +4,17 @@ namespace GustavPHP\Gustav\CLI;
 
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\{InputArgument, InputInterface};
+use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 #[AsCommand(name: 'installed', hidden: true)]
 class InstalledCommand extends Command
 {
-    protected static $defaultDescription = 'Starts development server.';
+    protected static $defaultDescription = 'Show post install instructions.';
 
     protected function configure(): void
     {
-        $this
-            ->setHelp('This command starts the development server.')
-            ->addArgument('address', InputArgument::OPTIONAL, 'Address to listen on', '0.0.0.0:4200')
-            ->addArgument('entrypoint', InputArgument::OPTIONAL, 'Entrypoint file', './app/index.php');
+        $this->setHelp('This command is run after installing the starter template..');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
@@ -28,7 +25,7 @@ class InstalledCommand extends Command
         $output->writeln('');
         $output->writeln('<info>    php gustav serve</info>');
         $output->writeln('');
-        $output->writeln('You can now visit the development server at <href=http://localhost:4200>http://localhost:4200</>');
+        $output->writeln('You can now visit the development server at <href=http://localhost:4201>http://localhost:4201</>');
         $output->writeln('');
 
         return Command::SUCCESS;
