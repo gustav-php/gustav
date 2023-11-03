b34a0b37cfa96a6e093b54980d2b9f0b79e4181d
Torsten Dittmann <torsten.dittmann@googlemail.com>
2023-11-03 01:31:55 +0100

    fix: dev cli

--- src/CLI/DevCommand.php
+++ src/CLI/DevCommand.php
@@ -4,16 +4,10 @@ namespace GustavPHP\Gustav\CLI;
 
 use function realpath;
 
-use RecursiveDirectoryIterator;
-use RecursiveIteratorIterator;
-use SplFileInfo;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\{InputArgument, InputInterface};
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Process\Process;
-
-use function usleep;
 
 #[AsCommand(name: 'dev')]
 class DevCommand extends Command
@@ -28,53 +22,10 @@ class DevCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $roadrunner = realpath(getcwd() . DIRECTORY_SEPARATOR . 'rr');
-        $command = escapeshellcmd("{$roadrunner} serve");
-        $latest = $this->getLatestModificationTimestamp();
-        $process = Process::fromShellCommandline($command);
-        $process->setTimeout(null);
-        $process->setIdleTimeout(null);
-        $process->start();
+        $command = escapeshellcmd("{$roadrunner} serve -d");
 
-        $counter = 0;
-        while (true) {
-            $output->write($process->getIncrementalOutput());
-            if (++$counter >= 100 && $this->getLatestModificationTimestamp() > $latest) {
-                $latest = $this->getLatestModificationTimestamp();
-                $output->writeln('<info>Changes detected, restarting server...</info>');
-                Process::fromShellCommandline(escapeshellcmd("{$roadrunner} --silent"))->run();
-            }
-
-            usleep(10000);
-            if ($process->isTerminated()) {
-                break;
-            }
-            if ($counter >= 100) {
-                $counter = 0;
-            }
-        }
+        passthru($command);
 
         return Command::SUCCESS;
-    }
-
-    protected function getLatestModificationTimestamp(): int
-    {
-        $root = getcwd() . DIRECTORY_SEPARATOR;
-        $latest = 0;
-        foreach (['src', 'app', 'views'] as $directory) {
-            $path = realpath($root . DIRECTORY_SEPARATOR . $directory);
-            if (!$path) {
-                continue;
-            }
-            foreach (new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path)) as $file) {
-                /**
-                 * @var SplFileInfo $file
-                 */
-                if ($file->getMTime() > $latest) {
-                    $latest = $file->getMTime();
-                }
-            }
-        }
-
-        return $latest;
     }
 }
