78f8cba211c9bda89439ad7d60d9f30cf0504149
Torsten Dittmann <torsten.dittmann@googlemail.com>
2023-11-03 02:48:40 +0100

    reduce test impact

--- tests/Integration/Tests/ResponsesTest.php
+++ tests/Integration/Tests/ResponsesTest.php
@@ -6,7 +6,7 @@ function startServer(): void
     stopServer();
     $command = 'nohup ' . getcwd() . DIRECTORY_SEPARATOR . 'rr serve -p -c .rr.tests.yaml 1> test.log &';
     exec($command);
-    sleep(5);
+    sleep(1);
 }
 
 function stopServer(): void
@@ -20,31 +20,19 @@ afterEach(fn () => stopServer());
 
 $client = new GuzzleHttp\Client(['base_uri' => 'http://127.0.0.1:5173']);
 
-test('can respond with plaintext', function () use ($client) {
+test('can respond', function () use ($client) {
     $response = $client->request('GET', '/responses/plaintext');
     expect($response->getBody()->getContents())->toBe('lorem ipsum');
     expect($response->getStatusCode())->toBe(200);
-});
-
-test('can respond with html', function () use ($client) {
     $response = $client->request('GET', '/responses/html');
     expect($response->getBody()->getContents())->toBe('<h1>lorem ipsum</h1>');
     expect($response->getStatusCode())->toBe(200);
-});
-
-test('can respond with xml', function () use ($client) {
     $response = $client->request('GET', '/responses/xml');
     expect($response->getBody()->getContents())->toBe('<root><lorem>ipsum</lorem></root>');
     expect($response->getStatusCode())->toBe(200);
-});
-
-test('can respond with json', function () use ($client) {
     $response = $client->request('GET', '/responses/json');
     expect($response->getBody()->getContents())->toBe('{"string":"lorem ipsum","number":123,"boolean":true,"null":null,"array":["lorem","ipsum","dolor","sit","amet"],"object":{"lorem":"ipsum","dolor":"sit","amet":"consectetur"}}');
     expect($response->getStatusCode())->toBe(200);
-});
-
-test('can respond with redirect', function () use ($client) {
     $response = $client->request('GET', '/responses/redirect');
     expect($response->getBody()->getContents())->toBe('lorem ipsum');
     expect($response->getStatusCode())->toBe(200);
